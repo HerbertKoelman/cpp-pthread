8008e65c248d2de7fb9897706a5ecad3a9d9afa9
Herbert Koelman <herbert.koelman@me.com>
2016-04-10 14:50:33 +0200

    issue #24
    update doxygen.

--- doc/html/search/all_3.js
+++ doc/html/search/all_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['join',['join',['../classpthread_1_1thread.html#a93c4105becf4dea1b00efb30cd7228af',1,'pthread::thread::join()'],['../classpthread_1_1thread__group.html#a39937a77e1059e352c9b39407a866f6e',1,'pthread::thread_group::join()']]],
+  ['join',['join',['../classpthread_1_1thread.html#a93c4105becf4dea1b00efb30cd7228af',1,'pthread::thread::join()'],['../classpthread_1_1abstract__thread.html#aedac81bb9eb76ba92c49c48d797ea25b',1,'pthread::abstract_thread::join()'],['../classpthread_1_1thread__group.html#a39937a77e1059e352c9b39407a866f6e',1,'pthread::thread_group::join()']]],
   ['joinable',['joinable',['../classpthread_1_1thread.html#a14b07d05a78157bcb5ee42fc8dd17e35',1,'pthread::thread']]]
 ];
--- doc/html/search/functions_a.js
+++ doc/html/search/functions_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['start',['start',['../classpthread_1_1thread__group.html#aaba00cf80d72cd986526384482457968',1,'pthread::thread_group']]],
+  ['start',['start',['../classpthread_1_1abstract__thread.html#ab121718028f3ca68d45db84d10ff2a3a',1,'pthread::abstract_thread::start()'],['../classpthread_1_1thread__group.html#aaba00cf80d72cd986526384482457968',1,'pthread::thread_group::start()']]],
   ['status',['status',['../classpthread_1_1thread.html#a3e49c7d73cb8411258790ed2fc0cd818',1,'pthread::thread']]]
 ];
