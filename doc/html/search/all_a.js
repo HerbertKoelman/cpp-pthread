2d9f1bd0a1ad698afd2b31871d98e15bbbc24d98
Herbert Koelman <herbert.koelman@pmu.fr>
2016-05-20 17:46:41 +0200

    updated documentation for version 1.3.0

--- doc/html/search/all_a.js
+++ doc/html/search/all_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_3d',['operator=',['../classpthread_1_1thread.html#a09903f6b3f396f2ea046c9c1606f0f61',1,'pthread::thread::operator=(const thread &amp;)=delete'],['../classpthread_1_1thread.html#a8f9ab7d43ad9aee0368ccc4778cf40d0',1,'pthread::thread::operator=(thread &amp;&amp;other)']]]
+  ['operator_3d',['operator=',['../classpthread_1_1lock__guard.html#a1f9ab705f7ffe9eb8739ff3cf34cf7f2',1,'pthread::lock_guard::operator=()'],['../classpthread_1_1thread.html#a09903f6b3f396f2ea046c9c1606f0f61',1,'pthread::thread::operator=(const thread &amp;)=delete'],['../classpthread_1_1thread.html#a8f9ab7d43ad9aee0368ccc4778cf40d0',1,'pthread::thread::operator=(thread &amp;&amp;other)']]]
 ];
--- doc/html/search/all_b.js
+++ doc/html/search/all_b.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['pthread',['pthread',['../namespacepthread.html',1,'']]],
-  ['pthread_5ferrmsg',['pthread_errmsg',['../classpthread_1_1pthread__exception.html#aa2371eb23800817723174186450e9716',1,'pthread::pthread_exception']]],
-  ['pthread_5ferrno',['pthread_errno',['../classpthread_1_1pthread__exception.html#a8a5c22a403ebd14635e912295fe02a1e',1,'pthread::pthread_exception']]],
+  ['pthread_5ferrmsg',['pthread_errmsg',['../classpthread_1_1pthread__exception.html#afc17488e8f6160865f1beb4d6fc20d2a',1,'pthread::pthread_exception']]],
+  ['pthread_5ferrno',['pthread_errno',['../classpthread_1_1pthread__exception.html#a4a869173054faca1945ac1a7729082d6',1,'pthread::pthread_exception']]],
   ['pthread_5fexception',['pthread_exception',['../classpthread_1_1pthread__exception.html',1,'pthread']]],
-  ['pthread_5fexception',['pthread_exception',['../classpthread_1_1pthread__exception.html#a157d4379b6eddcc82ca504534c51abe6',1,'pthread::pthread_exception']]],
+  ['pthread_5fexception',['pthread_exception',['../classpthread_1_1pthread__exception.html#aa3f5550d956b649cb554092595d7eff0',1,'pthread::pthread_exception::pthread_exception(const char *message, const int pthread_errno=0)'],['../classpthread_1_1pthread__exception.html#a157d4379b6eddcc82ca504534c51abe6',1,'pthread::pthread_exception::pthread_exception(const string message, const int pthread_errno=0)']]],
   ['this_5fthread',['this_thread',['../namespacepthread_1_1this__thread.html',1,'pthread']]]
 ];
