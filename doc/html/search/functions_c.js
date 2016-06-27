f25b7e5a232d168a7acbc3c0faaaf41c36321215
Herbert Koelman <herbert.koelman@pmu.fr>
2016-06-27 18:27:08 +0200

    doxygen v1.4.0

--- doc/html/search/functions_c.js
+++ doc/html/search/functions_c.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['size',['size',['../classpthread_1_1thread__group.html#a4b1a04ee731bba608193b089cc1d50a1',1,'pthread::thread_group']]],
-  ['sleep',['sleep',['../namespacepthread_1_1this__thread.html#a49f310463af9e4135f2012cc21dce375',1,'pthread::this_thread']]],
+  ['sleep_5ffor',['sleep_for',['../namespacepthread_1_1this__thread.html#a01ae1b738d3d2dbbfe966b4aad07a0a9',1,'pthread::this_thread']]],
   ['start',['start',['../classpthread_1_1abstract__thread.html#ab121718028f3ca68d45db84d10ff2a3a',1,'pthread::abstract_thread::start()'],['../classpthread_1_1thread__group.html#aaba00cf80d72cd986526384482457968',1,'pthread::thread_group::start()']]],
   ['status',['status',['../classpthread_1_1thread.html#a3e49c7d73cb8411258790ed2fc0cd818',1,'pthread::thread']]]
 ];
