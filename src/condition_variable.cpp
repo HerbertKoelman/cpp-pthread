7b3d43c63c58966c20becce6cec0adc614718304
herbert koelman <herbert.koelman@me.com>
2019-07-28 16:16:57 +0200

    refs #162 @1h
    The throw clause is deprecated and is therefore removed

--- src/condition_variable.cpp
+++ src/condition_variable.cpp
@@ -90,16 +90,13 @@ namespace pthread {
     }
   }
 
-  condition_variable::~condition_variable () throw(condition_variable_exception) {
-//      pthread_cond_destroy(&_condition);
-/* NOSONAR */
+  condition_variable::~condition_variable () { // throw(condition_variable_exception) {
     int rc = pthread_cond_destroy(&_condition);
     if (rc != 0){
       // This is the only way to signal that the destruction of the ressource failed, so the rule "Destructors should not throw exceptions" is therfore ignored.
       //
       throw condition_variable_exception("pthread condition variable destroy failed.", rc); // NOSONAR we decided to make an exception for this one
     }
-/**/
   }
 
 } // namespace pthread
